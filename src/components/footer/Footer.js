0a141059383d36a035963b4d5ee228496d252c22
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-12 17:06:23 +0530

    warnings resolve

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,7 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
 
 const FooterContent = styled.footer``;
 
-const FooterDiv = styled.div`
-  width: 100%;
-  height: 1px;
-  margin-bottom: 10px;
-  background-color: #db62ea;
-`;
-
 export const Footer = () => {
-  return <FooterContent>{/* <FooterDiv /> */}</FooterContent>;
+  return <FooterContent></FooterContent>;
 };
