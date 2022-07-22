d1a3f3ddf02103cd69bc8def41a1b4241e56f9db
lalitlsg <lalit.garghate@gmail.com>
2022-07-22 20:29:58 +0530

    noopener added

--- src/components/Main/Contents/home-contents/Resume.js
+++ src/components/Main/Contents/home-contents/Resume.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import resume from '../../../../images/resume.png'
-import style from '../../../../styles/home-styles/resume.module.css'
+import React from "react";
+import resume from "../../../../images/resume.png";
+import style from "../../../../styles/home-styles/resume.module.css";
 
 const Resume = () => {
   return (
     <div className={style.resumeMain}>
-        <img src={resume} alt="lalit's resume" className={style.resume}/>
-        <div className={style.preview}></div>
-        <a href='https://drive.google.com/file/d/1rfyHEMBzgsqMRPNgW2MTLtbtE7b_YWAL/view' target="_blank" className={style.viewFull}>View Full Resume</a>
+      <img src={resume} alt="lalit's resume" className={style.resume} />
+      <div className={style.preview}></div>
+      <a
+        href="https://drive.google.com/file/d/1rfyHEMBzgsqMRPNgW2MTLtbtE7b_YWAL/view"
+        rel="noreferrer"
+        target="_blank"
+        className={style.viewFull}
+      >
+        View Full Resume
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
